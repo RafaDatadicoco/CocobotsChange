4908634a94e27ad61e8476a89ca1c69cb73f2340
Rafa Guerrero <rafa.guerrero@datadicoco.com>
2026-04-22 10:40:08 -0400

    cambios de diseño

--- cocobots/settings.py
+++ cocobots/settings.py
@@ -82,14 +82,21 @@ WSGI_APPLICATION = 'cocobots.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "cocobots_db",
+#         "USER": "postgres",
+#         "PASSWORD": "mafe",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "cocobots_db",
-        "USER": "postgres",
-        "PASSWORD": "mafe",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
